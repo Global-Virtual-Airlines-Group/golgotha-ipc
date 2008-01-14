44ba2d13858b4498c88df41190cd7962a8a2238d
Luke Kolin <luke@deltava.org>
2008-01-14 04:25:10 +0000

    Add beta build support
    git-svn-id: file:///home/svn/IPC@4899 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/org/gvagroup/acars/ACARSClientInfo.java
+++ src/java/org/gvagroup/acars/ACARSClientInfo.java
@@ -1,4 +1,4 @@
-// Copyright 2007 Global Virtual Airlines Group. All Rights Reserved.
+// Copyright 2007, 2008 Global Virtual Airlines Group. All Rights Reserved.
 package org.gvagroup.acars;
 
 import java.util.*;
@@ -6,7 +6,7 @@ import java.util.*;
 /**
  * A bean to store ACARS client build information.
  * @author Luke
- * @version 1.1
+ * @version 2.1
  * @since 1.1
  */
 
@@ -14,10 +14,12 @@ public class ACARSClientInfo {
 	
 	private int _latest;
 	private final Map<String, Integer> _minBuilds = new TreeMap<String, Integer>();
+	private final Map<Integer, Integer> _betaBuilds = new TreeMap<Integer, Integer>();
 	
 	/**
 	 * Returns the latest ACARS client build.
 	 * @return the build number
+	 * @see ACARSClientInfo#setLatest(int)
 	 */
 	public int getLatest() {
 		return _latest;
@@ -32,9 +34,18 @@ public class ACARSClientInfo {
 	}
 	
 	/**
+	 * Returns the supported builds with betas.
+	 * @return a Collection of builds
+	 */
+	public Collection<Integer> getBetas() {
+		return new TreeSet<Integer>(_betaBuilds.keySet());
+	}
+	
+	/**
 	 * Returns the minimum supported build for a client version.
 	 * @param ver the client version
 	 * @return the build number
+	 * @see ACARSClientInfo#setMinimumBuild(String, int)
 	 */
 	public int getMinimumBuild(String ver) {
 		Integer build = _minBuilds.get(ver);
@@ -42,8 +53,19 @@ public class ACARSClientInfo {
 	}
 	
 	/**
+	 * Returns the minimum supported beta release for a build.
+	 * @param build the build number
+	 * @return the minimum beta release
+	 */
+	public int getMinimumBetaBuild(int build) {
+		Integer beta = _betaBuilds.get(Integer.valueOf(build));
+		return (beta == null) ? 0 : beta.intValue();
+	}
+	
+	/**
 	 * Sets the latest ACARS client build number.
 	 * @param build the build number
+	 * @see ACARSClientInfo#getLatest()
 	 */
 	public void setLatest(int build) {
 		_latest = build;
@@ -53,9 +75,20 @@ public class ACARSClientInfo {
 	 * Sets the minimum supported build for a client version.
 	 * @param ver the client version in major.minor format
 	 * @param build the build number
+	 * @see ACARSClientInfo#getMinimumBuild(String)
 	 */
 	public void setMinimumBuild(String ver, int build) {
-		_minBuilds.put(ver, new Integer(build));
+		_minBuilds.put(ver, Integer.valueOf(build));
+	}
+	
+	/**
+	 * Sets the minimum supported beta release for a build.
+	 * @param build the build number
+	 * @param beta the beta number
+	 */
+	public void setMinimumBetaBuild(int build, int beta) {
+		if (build > 0)
+			_betaBuilds.put(Integer.valueOf(build), Integer.valueOf(beta));
 	}
 	
 	public String toString() {
@@ -63,6 +96,8 @@ public class ACARSClientInfo {
 		buf.append(_latest);
 		buf.append(", min=");
 		buf.append(_minBuilds.toString());
+		buf.append(", beta=");
+		buf.append(_betaBuilds.toString());
 		return buf.toString();
 	}
 }
