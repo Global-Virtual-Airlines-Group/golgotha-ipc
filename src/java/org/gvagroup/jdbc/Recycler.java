bddc2470b9fe06a3c70b395f3c2e3cfc4b21ec46
Luke Kolin <luke@deltava.org>
2024-09-22 14:28:33 +0000

    Add recycler interface for 3.x
    git-svn-id: file:///home/svn/IPC@20276 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ src/java/org/gvagroup/jdbc/Recycler.java
@@ -0,0 +1,20 @@
+// Copyright 2024 Global Virtual Airlines Group. All Rights Reserved.
+package org.gvagroup.jdbc;
+
+/**
+ * An interface to mark objects that can cycle pooled connections.
+ * @author Luke
+ * @version 2.71
+ * @param <T> the Connection class
+ * @since 2.71
+ */
+
+interface Recycler<T> {
+
+	/**
+	 * Returns a connection to the pool.
+	 * @param c the Connection
+	 * @return the number of milliseconds the connection was used for
+	 */
+	public long release(T c);
+}
