533bc3a4f1b3333fd86d40fe3697526addf16791
Luke Kolin <luke@deltava.org>
2024-12-29 16:10:57 +0000

    Add use count comparison to test case
    git-svn-id: file:///home/svn/IPC@20538 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/test/org/gvagroup/pool/TestPoolEntryComparator.java
+++ src/test/org/gvagroup/pool/TestPoolEntryComparator.java
@@ -1,6 +1,7 @@
 package org.gvagroup.pool;
 
 import java.util.Properties;
+import java.util.concurrent.*;
 
 import junit.framework.TestCase;
 
@@ -8,10 +9,22 @@ public class TestPoolEntryComparator extends TestCase {
 
 	private final PoolEntryComparator _cmp = new PoolEntryComparator();
 	
+	@SuppressWarnings("serial")
+	private static class MockJDBCPoolEntry extends JDBCPoolEntry {
+		
+		MockJDBCPoolEntry(int id, int useCount) {
+			super(id, null, new Properties());
+			for (int x = 0; x < useCount; x++)
+				markUsed();
+			
+			markFree();
+		}
+	}
+	
 	public void testPersistent() {
 		
-		JDBCPoolEntry e1 = new JDBCPoolEntry(1, null, new Properties());
-		JDBCPoolEntry e2 = new JDBCPoolEntry(2, null, new Properties());
+		JDBCPoolEntry e1 = new MockJDBCPoolEntry(1, 0);
+		JDBCPoolEntry e2 = new MockJDBCPoolEntry(2, 0);
 		
 		assertFalse(e1.isDynamic());
 		assertFalse(e2.isDynamic());
@@ -21,9 +34,9 @@ public class TestPoolEntryComparator extends TestCase {
 	
 	public void testDynamic() {
 		
-		JDBCPoolEntry e1 = new JDBCPoolEntry(1, null, new Properties());
+		JDBCPoolEntry e1 = new MockJDBCPoolEntry(1, 0);
 		e1.setDynamic(true);
-		JDBCPoolEntry e2 = new JDBCPoolEntry(2, null, new Properties());
+		JDBCPoolEntry e2 = new MockJDBCPoolEntry(2, 0);
 		e2.setDynamic(true);
 		
 		assertTrue(e1.isDynamic());
@@ -34,14 +47,51 @@ public class TestPoolEntryComparator extends TestCase {
 
 	public void testPersistentDynamic() {
 		
-		JDBCPoolEntry e1 = new JDBCPoolEntry(1, null, new Properties());
+		JDBCPoolEntry e1 = new MockJDBCPoolEntry(1, 0);
 		e1.setDynamic(true);
-		JDBCPoolEntry e2 = new JDBCPoolEntry(2, null, new Properties());
+		JDBCPoolEntry e2 = new MockJDBCPoolEntry(2, 0);
 		e2.setDynamic(false);
 
 		assertTrue(e1.isDynamic());
 		assertFalse(e2.isDynamic());
 		assertEquals(1, _cmp.compare(e1, e2));
 		assertEquals(-1, _cmp.compare(e2, e1));
+	}
+	
+	public void testUsageCount() {
+		
+		JDBCPoolEntry e1 = new MockJDBCPoolEntry(1, 20);
+		JDBCPoolEntry e2 = new MockJDBCPoolEntry(2, 10);
+		
+		assertEquals(20, e1.getUseCount());
+		assertEquals(10, e2.getUseCount());
+		assertFalse(e1.isDynamic());
+		assertFalse(e2.isDynamic());
+		assertTrue(e2.getUseCount() < e1.getUseCount());
+		assertEquals(-1, e1.compareTo(e2));
+		assertEquals(1, _cmp.compare(e1, e2));
+	}
+	
+	public void testQueue() {
+		
+		BlockingQueue<ConnectionPoolEntry<?>> _idleCons = new PriorityBlockingQueue<ConnectionPoolEntry<?>>(4, new PoolEntryComparator());
+		
+		JDBCPoolEntry e1 = new MockJDBCPoolEntry(1, 20);
+		JDBCPoolEntry e2 = new MockJDBCPoolEntry(2, 10);
+		
+		assertEquals(20, e1.getUseCount());
+		assertEquals(10, e2.getUseCount());
+		assertFalse(e1.isDynamic());
+		assertFalse(e2.isDynamic());
+		assertTrue(e2.getUseCount() < e1.getUseCount());
+		assertEquals(1, _cmp.compare(e1, e2));
+
+		_idleCons.offer(e1);
+		_idleCons.offer(e2);
+		assertEquals(2, _idleCons.size());
+		
+		ConnectionPoolEntry<?> e3 = _idleCons.poll();
+		assertNotNull(e3);
+		assertEquals(2, e3.getID());
 	}
 }
