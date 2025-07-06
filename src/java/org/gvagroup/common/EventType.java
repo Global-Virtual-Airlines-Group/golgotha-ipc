633163741a464f2108e9a1d7308f357a6263471a
Luke Kolin <luke@deltava.org>
2025-07-06 16:26:34 -0400

    Add FLIGHT_REPORT event type for PIREP notifications

--- src/java/org/gvagroup/common/EventType.java
+++ src/java/org/gvagroup/common/EventType.java
@@ -1,11 +1,12 @@
+// Copyright 2021, 2025 Global Virtual Airlines Group. All Rights Reserved.
 package org.gvagroup.common;
 
 /**
  * An enumeration of IPC Event types.
- * @version 2.33
+ * @version 3.11
  * @since 1.0
  */
 
 public enum EventType {
-	AIRPORT_RELOAD, AIRLINE_RELOAD, USER_SUSPEND, USER_INVALIDATE, MVS_RELOAD, TZ_RELOAD, CACHE_FLUSH, CACHE_STATS, AIRPORT_RENAME, AIRCRAFT_RENAME;
+	AIRPORT_RELOAD, AIRLINE_RELOAD, USER_SUSPEND, USER_INVALIDATE, TZ_RELOAD, CACHE_FLUSH, CACHE_STATS, AIRPORT_RENAME, AIRCRAFT_RENAME, FLIGHT_REPORT;
 }
